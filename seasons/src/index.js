261aef65e5824bdd5f023c2aa34d4988b01d56a1
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-08-20 01:40:24 +0200

    Section 5: State in React Components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.log(error)
-    );
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: "" };
 
-    return <div>Latitude: </div>
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat)
+      return <div>Error: {this.state.errorMessage}</div>;
+
+    if (!this.state.errorMessage && this.state.lat)
+      return <div>Latitude: {this.state.lat}</div>;
+
+    return <div>Loading...</div>;
   }
 }
 
