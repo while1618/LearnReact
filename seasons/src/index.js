7652260cf1174f71b8172b9713a27a68160ceafe
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-08-21 00:00:55 +0200

    Section 6: Understanding Lifecycle Methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,25 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMessage: "" };
+  state = { lat: null, errorMessage: "" }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
       (error) => this.setState({ errorMessage: error.message })
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat)
       return <div>Error: {this.state.errorMessage}</div>;
 
     if (!this.state.errorMessage && this.state.lat)
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
 
-    return <div>Loading...</div>;
+    return <Loader message="Please accept location request" />;
+  }
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
