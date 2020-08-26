c3938b8da73d374dd43bd7ee91bf9bdb6e7086c6
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-08-26 03:08:19 +0200

    Section 12: Understanding Hooks in React
    Accordion

---
+++ widgets/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React.js is an open-source JavaScript library that is used for building user interfaces."
+  },
+  {
+    title: "Why use React?",
+    content: "React allows developers to create large web applications that can change data, without reloading the page. The main purpose of React is to be fast, scalable, and simple."
+  },
+  {
+    title: "How created React?",
+    content: "React was originally created by Jordan Walke. Today, React has over a thousand open source contributors."
+  }
+]
+
+export default () => {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  )
+}
