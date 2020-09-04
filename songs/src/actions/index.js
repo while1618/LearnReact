2e1dbee66679f56eeeed3268f0a057f8c1a40236
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-09-04 23:40:24 +0200

    Section 17: Integrating React with Redux

--- songs/src/actions/index.js
+++ songs/src/actions/index.js
@@ -1,4 +1,4 @@
-export const selectSong = () => {
+export const selectSong = (song) => {
   return {
     type: "SONG_SELECTED",
     payload: song
--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,12 +1,34 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
-const SongList = () => {
+const SongList = ({ songs, selectSong }) => {
+  const renderSongs = () => {
+    return songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary" onClick={() => selectSong(song)}>Select</button>
+          </div>
+          <div className="content">
+            {song.title}
+          </div>
+        </div>
+      );
+    });
+  }
+
   return (
-    <div>
-      song list
+    <div className="ui divided list">
+      {renderSongs()}
     </div>
   );
 };
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+  return {
+    songs: state.songs
+  };
+};
+
+export default connect(mapStateToProps, { selectSong })(SongList);
