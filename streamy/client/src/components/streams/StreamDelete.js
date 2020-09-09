7eb5e288c6fbc3e4711284d21ae8e482390709ec
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-09-10 01:10:37 +0200

    Section 25: Using React Portals
    Deleting stream in modal

--- streamy/client/src/components/streams/StreamDelete.js
+++ streamy/client/src/components/streams/StreamDelete.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const StreamDelete = () => {
+import Modal from "../Modal";
+import history from "../../history";
+import { fetchStream, deleteStream } from "../../actions";
+
+const StreamDelete = ({ fetchStream, deleteStream, match, stream }) => {
+  useEffect(() => {
+    fetchStream(match.params.id);
+  }, [fetchStream, match.params.id]);
+
+  const renderActions = () => {
+    return (
+      <Fragment>
+        <button onClick={() => deleteStream(match.params.id)} className="ui button negative">Delete</button>
+        <Link to="/" className="ui button">Cancel</Link>
+      </Fragment>
+    );
+  };
+
+  const renderContent = () => {
+    if (!stream)
+      return "Loading...";
+    return `Are you sure you want to delete stream with title: ${stream.title}`;
+  }
+
   return (
-    <div>
-      Stream Delete
-    </div>
+    <Modal
+      title="Delete a Stream"
+      content={renderContent()}
+      actions={renderActions()}
+      onDismiss={() => history.push("/")}
+    />
   );
 };
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
