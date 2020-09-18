184683c1dd62734a0d87415b823e22f2a862e41b
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-09-19 00:49:31 +0200

    absolute path

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import App from "../App";
-import CommentBox from "../CommentBox";
-import CommentList from "../CommentList";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
 it("show's a CommentBox", () => {
   const wrapper = shallow(<App />);
