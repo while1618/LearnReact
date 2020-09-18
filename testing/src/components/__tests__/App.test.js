52e9fa8cb9bced23d44f98bb04d0044eff2a2bfa
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-09-19 00:09:59 +0200

    shallow function

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { shallow } from "enzyme";
 
 import App from "../App";
+import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 it("show's a CommentBox", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(CommentBox).length).toEqual(1);
+});
 
+it("show's a CommentList", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(CommentList).length).toEqual(1);
 });
